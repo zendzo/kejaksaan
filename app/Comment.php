42279911704c2b0af19b3248cef250b5213601c9
zendzo <zendzzo@gmail.com>
2018-08-15 15:26:17 +0700

    add belong to user relationship

--- app/Comment.php
+++ app/Comment.php
@@ -6,10 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    protected $fillable = ['body'];
+    protected $fillable = ['body','user_id'];
     
     public function commentable()
     {
         return $this->morphTo();
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
