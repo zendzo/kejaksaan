e7c0e473b9797705a653b016ab10bd711e43a48c
zendzo <zendzzo@gmail.com>
2018-08-15 16:19:11 +0700

    increase varchar to 255

--- database/migrations/2018_05_22_034236_create_roles_table.php
+++ database/migrations/2018_05_22_034236_create_roles_table.php
@@ -15,7 +15,7 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name',20);
+            $table->string('name');
             $table->timestamps();
         });
     }
