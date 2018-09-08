7d73c65859e2c6163af169d533c54f0b32ffc944
zendzo <zendzzo@gmail.com>
2018-09-08 21:24:06 +0700

    seed data unable

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleTableSeeder::class);
         $this->call(UserTableSeeder::class);
         $this->call(GenderTableSeeder::class);
-        $this->call(PengaduanTableSeeder::class);
+        // $this->call(PengaduanTableSeeder::class);
     }
 }
